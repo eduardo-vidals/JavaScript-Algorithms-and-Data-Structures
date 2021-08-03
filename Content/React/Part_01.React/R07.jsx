336d14bf52c55e60c45f3f38be55559b855a9c11
Eduardo Vidals <eduardovidals69@gmail.com>
2021-08-02 23:05:19 -0400

    React content files

---
+++ Content/React/Part_01.React/R07.jsx
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+    // Change code below this line
+    return (
+        <div>
+            <p> Some text </p>
+        </div>
+    );
+    // Change code above this line
+}
