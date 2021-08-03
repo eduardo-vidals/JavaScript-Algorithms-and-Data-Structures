72986bad954cb162c14a38e5676d89a59a10c3ae
Eduardo Vidals <eduardovidals69@gmail.com>
2021-08-02 23:03:11 -0400

    Javascript content files

--- Content/React/Part_01.React/R02.jsx
+++
@@ -1,9 +0,0 @@
-const JSX = <div>
-    <h1>Header</h1>
-    <p>Some text</p>
-    <ul>
-        <li> Item one </li>
-        <li> Item two </li>
-        <li> Item three </li>
-    </ul>
-</div>
--- Content/React/Part_01.React/R07.jsx
+++
@@ -1,9 +0,0 @@
-const MyComponent = function() {
-    // Change code below this line
-    return (
-        <div>
-            <p> Some text </p>
-        </div>
-    );
-    // Change code above this line
-}
