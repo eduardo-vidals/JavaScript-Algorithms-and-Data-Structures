8464bd19e71f38db3e2da699970d51b4a49c4c5b
Eduardo Vidals <63515838+eduardo-vidals@users.noreply.github.com>
2021-08-05 00:05:08 -0400

    React content files

--- Content/React/Part_01.React/R47.jsx
+++ Content/React/Part_01.React/R47.jsx
@@ -1,11 +1,11 @@
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <div/>
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />
+  }
 };
 
 // Change code below this line
-ReactDOMServer.renderToString();
+ReactDOMServer.renderToString(<App/>);
